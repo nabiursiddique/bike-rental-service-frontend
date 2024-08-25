10355a6eb1d7e27d9163317af2132d815c54eb7f
Nabiur siddique <nabiursiddique01@gmail.com>
2024-08-25 13:17:21 +0600

    basic redux setup is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { Button } from './components/ui/button';
+import { decrement, increment } from './redux/features/counter/counterSlice';
+import { useAppDispatch, useAppSelector } from './redux/hooks';
+import { RootState } from './redux/store';
 
 const App = () => {
+  const count = useAppSelector((state: RootState) => state.counter.value);
+  const dispatch = useAppDispatch();
   return (
     <div>
-      <Button>Hello Shadcn</Button>
+      <Button onClick={() => dispatch(increment())}>Increment</Button>
+      <h1>{count}</h1>
+      <Button onClick={() => dispatch(decrement())}>Decrement</Button>
     </div>
   );
 };
