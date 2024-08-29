331ea4b57876cd7a23c5d78df6a6ed685cc5e3c6
Nabiur siddique <nabiursiddique01@gmail.com>
2024-08-30 01:30:12 +0600

    only available bikes will show on featured bikes

--- src/components/HomeComponents/FeaturedBikes/FeaturedBikes.tsx
+++ src/components/HomeComponents/FeaturedBikes/FeaturedBikes.tsx
@@ -9,18 +9,22 @@ import bikeLogo from '../../../assets/icons/Bike logo.png';
 import { useGetAllBikesQuery } from '@/redux/features/bikes/bikes.api';
 import BikeCard from '@/components/BikeCard/BikeCard';
 import Loading from '@/components/CommonComponents/Loading';
+import { TBike } from '@/types/bikeType';
 
 const FeaturedBikes = () => {
   const { data, isLoading } = useGetAllBikesQuery(undefined);
+
   if (isLoading) {
     return <Loading />;
   }
-  const bikes = data?.data;
+
+  const availableBikes = data?.data?.filter((bike: TBike) => bike.isAvailable);
+
   return (
     <div className='mt-20 font-[oswald] lg:px-20'>
       <div>
         <div className='flex justify-center'>
-          <img src={bikeLogo} alt='' />
+          <img src={bikeLogo} alt='Bike Logo' />
         </div>
         <h1 className='lg:text-5xl text-3xl font-semibold uppercase lg:mt-5 text-center'>
           Featured Bikes
@@ -30,51 +34,47 @@ const FeaturedBikes = () => {
       <div className='my-10'>
         <Carousel className='lg:w-full w-[90%] mx-auto'>
           <CarouselContent className='-ml-1'>
-            {isLoading
-              ? Array(3)
-                  .fill(0)
-                  .map((_, index) => (
-                    <CarouselItem
-                      key={index}
-                      className='pl-1 md:basis-1/2 lg:basis-1/3'
-                    ></CarouselItem>
-                  ))
-              : bikes &&
-                bikes.map(
-                  (
-                    bike: {
-                      _id: string;
-                      name: string;
-                      description: string;
-                      image: string;
-                      isAvailable: boolean;
-                      brand: string;
-                      model: string;
-                      year: number;
-                      cc: number;
-                      pricePerHour: number;
-                    },
-                    index: string
-                  ) => (
-                    <CarouselItem
-                      key={index}
-                      className='pl-1 md:basis-1/2 lg:basis-1/3'
-                    >
-                      <BikeCard
-                        id={bike._id}
-                        bikeName={bike.name}
-                        image={bike.image}
-                        availability={bike.isAvailable}
-                        description={bike.description}
-                        brand={bike.brand}
-                        model={bike.model}
-                        year={bike.year}
-                        maxSpeed={bike.cc}
-                        price={bike.pricePerHour}
-                      />
-                    </CarouselItem>
-                  )
-                )}
+            {availableBikes && availableBikes.length > 0 ? (
+              availableBikes.map(
+                (
+                  bike: {
+                    _id: string;
+                    name: string;
+                    description: string;
+                    image: string;
+                    isAvailable: boolean;
+                    brand: string;
+                    model: string;
+                    year: number;
+                    cc: number;
+                    pricePerHour: number;
+                  },
+                  index: string
+                ) => (
+                  <CarouselItem
+                    key={index}
+                    className='pl-1 md:basis-1/2 lg:basis-1/3'
+                  >
+                    <BikeCard
+                      id={bike._id}
+                      bikeName={bike.name}
+                      image={bike.image}
+                      availability={bike.isAvailable}
+                      description={bike.description}
+                      brand={bike.brand}
+                      model={bike.model}
+                      year={bike.year}
+                      maxSpeed={bike.cc}
+                      price={bike.pricePerHour}
+                    />
+                  </CarouselItem>
+                )
+              )
+            ) : (
+              <p className='text-center w-full'>
+                No available bikes at the moment.
+              </p>
+            )}
           </CarouselContent>
           <CarouselPrevious className='ml-7 lg:ml-3' />
           <CarouselNext className='mr-7 lg:mr-3' />
